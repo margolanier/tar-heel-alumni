9d7373b086c428deed5cdabd1022550800a9794c
Margo Lanier <lanier.margo@gmail.com>
2015-04-29 09:40:22 -0400

    adds styles and other pages

--- js/maps-results.js
+++ js/maps-results.js
@@ -6,10 +6,7 @@ function initialize() {
   var mapOptions = {
     zoom: 7,
     center: latlng,
-    markerOptions: {
-                        iconUrl: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=2|EF75FE'
-                      },
-    styles: [{"stylers":[{"saturation":-100},{"gamma":1}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"water","stylers":[{"visibility":"on"},{"saturation":50},{"gamma":0},{"hue":"#50a5d1"}]},{"featureType":"administrative.neighborhood","elementType":"labels.text.fill","stylers":[{"color":"#333333"}]},{"featureType":"road.local","elementType":"labels.text","stylers":[{"weight":0.5},{"color":"#333333"}]},{"featureType":"transit.station","elementType":"labels.icon","stylers":[{"gamma":1},{"saturation":50}]}]
+    styles: [{"stylers":[{"saturation":-100},{"gamma":1}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"water","stylers":[{"visibility":"on"},{"saturation":50},{"gamma":0},{"hue":"#56a0d3"}]},{"featureType":"administrative.neighborhood","elementType":"labels.text.fill","stylers":[{"color":"#333333"}]},{"featureType":"road.local","elementType":"labels.text","stylers":[{"weight":0.5},{"color":"#333333"}]},{"featureType":"transit.station","elementType":"labels.icon","stylers":[{"gamma":1},{"saturation":70}]}]
   }
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   console.log('here');
@@ -19,7 +16,8 @@ function initialize() {
 function code_addresses() {
   var addresses = [
     'Chapel Hill, NC',
-    'Lumberton, NC'
+    'Lincolnton, NC',
+    'Raleigh, NC'
   ];
   for(var i=0; i < addresses.length; i++) {
       console.log('im in your loops')
@@ -33,7 +31,8 @@ function codeAddress(address) {
       map.setCenter(results[0].geometry.location);
       var marker = new google.maps.Marker({
           map: map,
-          position: results[0].geometry.location
+          position: results[0].geometry.location,
+          icon: '../img/marker.png'
       });
       console.log(results[0].formatted_address);
     } else {
