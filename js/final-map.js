287e87415b5a2d5e7241b84516e59e10bfdb3dbb
Margo Lanier <lanier.margo@gmail.com>
2015-05-01 14:51:14 -0400

    marker styles

--- js/final-map.js
+++ js/final-map.js
@@ -4,6 +4,7 @@ var map;
 var addresses = [];
 var person = [];
 var info = [];
+var contact = [];
 
 
 function initialize() {
@@ -22,7 +23,7 @@ function code_addresses(data, tabletop) {
  
 geocoder = new google.maps.Geocoder();
   var mapOptions = {
-    zoom: 4,
+    zoom: 5,
     center: new google.maps.LatLng(35.903928, -79.046243),
     styles: [{"stylers":[{"saturation":-100},{"gamma":1}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"water","stylers":[{"visibility":"on"},{"saturation":50},{"gamma":0},{"hue":"#56a0d3"}]},{"featureType":"administrative.neighborhood","elementType":"labels.text.fill","stylers":[{"color":"#333333"}]},{"featureType":"road.local","elementType":"labels.text","stylers":[{"weight":0.5},{"color":"#333333"}]},{"featureType":"transit.station","elementType":"labels.icon","stylers":[{"gamma":1},{"saturation":70}]}]
   }
@@ -34,23 +35,27 @@ geocoder = new google.maps.Geocoder();
     addresses.push(entry.citystate);
     person.push(entry.name);
     info.push(entry.companyorschool);
+    contact.push(entry.contact);
   })
 
 
   for(var i=0; i < addresses.length; i++) {
       console.log('inside "code_addresses" for loop')
-      codeAddress(addresses[i], person[i], info[i]);
+      codeAddress(addresses[i], person[i], info[i], contact[i]);
   }
 }
 
-function codeAddress(address, person, info) {
+function codeAddress(address, person, info, contact) {
   geocoder.geocode( { 'address': address}, function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
       map.setCenter(results[0].geometry.location);
 
       var contentString = '<div id="iw-container">' +
         '<div class="iw-title">' + person + '</div>' +
-        '<div class="iw-content"><p>' + info + '<br>' + address + '</p></div>' +
+        '<div class="iw-content"><p><i class="fa fw fa-briefcase"></i>' + info +
+        '<br><i class="fa fw fa-map-marker"></i>' + address +
+        '<br><i class="fa fw fa-paper-plane"></i>' + contact +
+        '</p></div>' +
         '</div>';
 
       var infowindow = new google.maps.InfoWindow({
