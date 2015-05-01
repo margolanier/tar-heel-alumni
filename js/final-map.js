13c28658297c751af5cfbbaa035e5b2a6827bf00
Margo Lanier <lanier.margo@gmail.com>
2015-05-01 13:57:17 -0400

    marker finished

--- js/final-map.js
+++ js/final-map.js
@@ -2,7 +2,9 @@
 var geocoder;
 var map;
 var addresses = [];
+var person = [];
 var info = [];
+
 
 function initialize() {
   var public_spreadsheet_url = 'https://docs.google.com/spreadsheet/pub?key=0AuSm4qaFIJGKdEY3NXhsbXE1ci1FaXRZb0pfRWJLQ2c&output=html';
@@ -19,10 +21,9 @@ function initialize() {
 function code_addresses(data, tabletop) {
  
 geocoder = new google.maps.Geocoder();
-  var latlng = new google.maps.LatLng(35.903928, -79.046243);
   var mapOptions = {
-    zoom: 7,
-    center: latlng,
+    zoom: 4,
+    center: new google.maps.LatLng(35.903928, -79.046243),
     styles: [{"stylers":[{"saturation":-100},{"gamma":1}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"poi.place_of_worship","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"water","stylers":[{"visibility":"on"},{"saturation":50},{"gamma":0},{"hue":"#56a0d3"}]},{"featureType":"administrative.neighborhood","elementType":"labels.text.fill","stylers":[{"color":"#333333"}]},{"featureType":"road.local","elementType":"labels.text","stylers":[{"weight":0.5},{"color":"#333333"}]},{"featureType":"transit.station","elementType":"labels.icon","stylers":[{"gamma":1},{"saturation":70}]}]
   }
   
@@ -31,24 +32,25 @@ geocoder = new google.maps.Geocoder();
 
   $.each( tabletop.sheets("Responses").all(), function(i, entry) {
     addresses.push(entry.citystate);
-    info.push(entry.name);
+    person.push(entry.name);
+    info.push(entry.companyorschool);
   })
 
 
   for(var i=0; i < addresses.length; i++) {
       console.log('inside "code_addresses" for loop')
-      codeAddress(addresses[i], info[i]);
+      codeAddress(addresses[i], person[i], info[i]);
   }
 }
 
-function codeAddress(address, info) {
+function codeAddress(address, person, info) {
   geocoder.geocode( { 'address': address}, function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
       map.setCenter(results[0].geometry.location);
 
       var contentString = '<div id="iw-container">' +
-        '<div class="iw-title">' + info + '</div>' +
-        '<div class="iw-content"><p>' + 'city, state' + '</p></div>' +
+        '<div class="iw-title">' + person + '</div>' +
+        '<div class="iw-content"><p>' + info + '<br>' + address + '</p></div>' +
         '</div>';
 
       var infowindow = new google.maps.InfoWindow({
